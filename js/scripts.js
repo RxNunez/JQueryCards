defe94c3bd47fd80e14b730a1744b0d9999ed346
Evan Johnson and Ryan Nunez <ryan@>
2017-07-05 10:34:56 -0700

    add 20 functional cards with drop capability

---
+++ js/scripts.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+  // ROW 1
+  $("#JS").click(function() {
+    $("#JS + .definition").toggle();
+  })
+  $("#operators").click(function() {
+    $("#operators + .definition").toggle();
+  })
+  $("#variables").click(function() {
+    $("#variables + .definition").toggle();
+  })
+  $("#namingConventions").click(function() {
+    $("#namingConventions + .definition").toggle();
+  })
+  // ROW 2
+  $("#functions").click(function() {
+    $("#functions + .definition").toggle();
+  })
+  $("#methods").click(function() {
+    $("#methods + .definition").toggle();
+  })
+  $("#arguments").click(function() {
+    $("#arguments + .definition").toggle();
+  })
+  $("#parameters").click(function() {
+    $("#parameters + .definition").toggle();
+  })
+  // ROW 3
+  $("#return").click(function() {
+    $("#return + .definition").toggle();
+  })
+  $("#chainingMethods").click(function() {
+    $("#chainingMethods + .definition").toggle();
+  })
+  $("#strings").click(function() {
+    $("#strings + .definition").toggle();
+  })
+  $("#booleans").click(function() {
+    $("#booleans + .definition").toggle();
+  })
+  // ROW 4
+  $("#escape").click(function() {
+    $("#escape + .definition").toggle();
+  })
+  $("#alert").click(function() {
+    $("#alert + .definition").toggle();
+  })
+  $("#undefined").click(function() {
+    $("#undefined + .definition").toggle();
+  })
+  $("#nan").click(function() {
+    $("#nan + .definition").toggle();
+  })
+  // ROW 5
+  $("#comments").click(function() {
+    $("#comments + .definition").toggle();
+  })
+  $("#jq").click(function() {
+    $("#jq + .definition").toggle();
+  })
+  $("#attributes").click(function() {
+    $("#attributes + .definition").toggle();
+  })
+  $("#faqs").click(function() {
+    $("#faqs + .definition").toggle();
+  })
+});
